7a69b7b1bdbcfcc6b3be968d138af1c468302682
Adam K. Sumner <asumner@hawk.iit.edu>
2017-05-14 02:17:14 -0400

    fix broken custom emoji

--- src/cogs/foam.py
+++ src/cogs/foam.py
@@ -45,13 +45,14 @@ class Foam:
     async def halp(self, ctx):
         await self.bot.say(
             "Waddup mai foam friendly friends?! Here's a list of my commands. I only do voice commands at the moment"
-            " but I plan to learn more things really soon :aaam:\n"
+            " but I plan to learn more things really soon :ok_hand:\n"
             "\nCOMMANDS:"
             "\n[--foam] Plays a sound related to foam and how much of it we has"
             "\n[--doctor] I'm a fucking doctor"
             "\n[--noises] Not very many, but they get the job done"
             "\n[--randum] An assortment of foam-related things without a specific category"
-            "\n[--memes] Just for you my darlings")
+            "\n[--memes] Just for you my darlings"
+        )
 
     async def on_message(self, message):
         if message.author.id != self.bot.user.id:
